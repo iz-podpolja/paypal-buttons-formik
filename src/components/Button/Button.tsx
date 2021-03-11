7960de10221239033baface09ed6843bd2ac3187
Kamil Piwko <iz.podpolja@gmail.com>
2021-03-11 10:14:49 +0100

    mixing bootstrap with tailwind and css modules

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,24 @@
+import classNames from 'classnames';
+import React, { ButtonHTMLAttributes, useMemo } from 'react';
+
+import TypedMemo from '../../utils/typedMemo';
+import css from './Button.module.css';
+
+function Button({
+  children: _,
+  className: className_,
+  ...props
+}: ButtonHTMLAttributes<HTMLButtonElement>): JSX.Element {
+  const className = useMemo(
+    () =>
+      classNames(css.button, className_, "tw-text-right", "tw-border-red-500"),
+    [className_]
+  );
+  return (
+    <button className={className} {...props}>
+      {className}
+    </button>
+  );
+}
+
+export default TypedMemo(Button);
---
+++ src/utils/typedMemo.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const TypedMemo: <T>(ele: T) => T = React.memo;
+
+export default TypedMemo;
